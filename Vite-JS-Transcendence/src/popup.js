07a9da2f6da871d9570ebf69a514fe15ec60c565
François GONZALEZ <francois.gnz@gmail.com>
2025-04-07 18:52:19 +0200

    register button now send inputs to backend

--- Vite-JS-Transcendence/src/popup.js
+++ Vite-JS-Transcendence/src/popup.js
@@ -12,9 +12,52 @@ export function registerPopup() {
         form.classList.add("active");
     })
 
-    submitBtn.addEventListener("click", (e) => {
+    form.addEventListener("submit", async (e) => {
+        // L'evenement submit sera actif seulement si les champs input email et password on ete remplis correctement , ici on pourra modifier la validation plus tard si on le souhaite.
+
+        const result = document.getElementById("result");
+
         console.log("Submit button clicked");
         e.preventDefault();
-        form.classList.remove("active");
+
+        //isFormValid(); Ici on pourrai check plus en details et ameliorer l'UX pour marquer les problemes.
+
+        //Permet de recuperer les values d'un form simplement dans une classe.
+        const formData = new FormData(form);
+        //Creer un simple object JS avec les valeurs de formData
+        const data = Object.fromEntries(formData);
+        console.log("Form data :",data);
+
+        try
+        {
+            const res = await fetch("https://reqres.in/api/users", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            });
+
+            if (!res.ok)
+            {
+                //Ici on peut gerer si la registration n'aboutie pas.
+                // const error = await res.text();
+                // console.log(error);
+                result.innerText = "Registration went wrong";
+                // throw new Error(error);
+            }
+
+            const responseData = await res.json();
+            console.log("Reponse du serveur:", responseData);
+            //Ici on peut gerer si la registration est bonne.
+            result.innerText = "Registration successfull";
+            // form.classList.remove("active");
+            // form.reset();
+        }
+        catch (err)
+        {
+            console.error("Erreur:", err);
+        }
     })
 }
+ 
--- Vite-JS-Transcendence/src/views/landing.js
+++ Vite-JS-Transcendence/src/views/landing.js
@@ -6,9 +6,10 @@ export function landingView() {
         <button id="registerBtn">REGISTER</button>
         <form id="popup-form" class="popup-form">
             REGISTER
-            <input type="text" name="email" id="email" placeholder="Email" required>
+            <input type="email" name="email" id="email" placeholder="Email" required>
             <input type="password" name="password" id="password" placeholder="Password" required>
             <button type="submit" id="register-submit">SUBMIT</button>
+            <div id="result"></div>
         </form>
     </div>
     `
